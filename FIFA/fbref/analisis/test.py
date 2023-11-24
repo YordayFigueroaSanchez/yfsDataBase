3ac53c78e8b8cd9f16eccbc2b517dc691e7ca4a0
yfsanchez <yfsanchez@gmail.com>
2023-11-24 16:40:13 -0300

    test de function con dos elementos en la respuesta

--- FIFA/fbref/analisis/test.py
+++ FIFA/fbref/analisis/test.py
@@ -1,15 +1,24 @@
 import os
 
-print('test')
-extension = '.json'
+# print('test')
+# extension = '.json'
+# # Obtén la ruta al directorio 'data' desde el directorio 'code'
+# ruta = os.path.join(os.path.dirname(__file__), '..', '1938', 'game', '20231120_1207', 'json')
+# # Ahora puedes acceder a los archivos en el directorio 'data'
+# archivos_json = [archivo for archivo in os.listdir(ruta) if archivo.endswith(extension)]
+# # archivo_datos = os.path.join(ruta_data, 'archivo_de_datos.txt')
+# for archivo in archivos_json:
+#     print(archivo)
 
-# Obtén la ruta al directorio 'data' desde el directorio 'code'
-ruta = os.path.join(os.path.dirname(__file__), '..', '1938', 'game', '20231120_1207', 'json')
-
-# Ahora puedes acceder a los archivos en el directorio 'data'
-archivos_json = [archivo for archivo in os.listdir(ruta) if archivo.endswith(extension)]
-
-# archivo_datos = os.path.join(ruta_data, 'archivo_de_datos.txt')
-
-for archivo in archivos_json:
-    print(archivo)
+def arreglo_y_conjunto():
+    # Crear un arreglo (lista)
+    arreglo = [1, 2, 3, 4, 5]
+    # Crear un conjunto a partir del arreglo
+    conjunto = set(arreglo)
+    # Retornar tanto el arreglo como el conjunto
+    return arreglo, conjunto
+# Llamamos a la función
+mi_arreglo, mi_conjunto = arreglo_y_conjunto()
+# Mostramos los resultados
+print("Arreglo:", mi_arreglo)
+print("Conjunto:", mi_conjunto)
